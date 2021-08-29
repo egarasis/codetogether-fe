81d5729cc8bb4479cece1a1b3b5d8eb88e966edf
egarasis <egarasis@gmail.com>
2021-08-29 12:11:05 +0700

    contoh tambah fungsi

--- src/view/Home.js
+++ src/view/Home.js
@@ -2,11 +2,17 @@ import React, {useState} from "react";
 import { Card, CardHeader, CardBody } from "reactstrap";
 
 const defaultBelanja = [
-    {barang: "Indomie Goreng", qty: 30, satuan: "Bungkus"},
-    {barang: "Sari Roti", qty: 1, satuan: "Bungkus"},
-    {barang: "Apel", qty: 5, satuan: "Buah"},
-    {barang: "Obat tikus", qty: 4, satuan: "Bungkus"}
+    {barang: "Indomie Goreng", qty: 30, satuan: "Bungkus", tempat: "Indomaret"},
+    {barang: "Sari Roti", qty: 1, satuan: "Bungkus", tempat: "Alfamidi"},
+    {barang: "Apel", qty: 5, satuan: "Buah", tempat: "Indomaret"},
+    {barang: "Obat tikus", qty: 4, satuan: "Bungkus", tempat: "Toko Mak Yusron"},
+    {barang: "Teh Botol", qty: 1, satuan: "Botol", tempat: "Indomaret"},
+
+    
 ]
+
+defaultBelanja[4].qty = defaultBelanja[0].qty * 2
+
 
 export default function Home(props){
     const [dataBelanja, setDataBelanja] = useState(defaultBelanja)
@@ -22,6 +28,8 @@ export default function Home(props){
                             {obj.barang}
                             {" | "}
                             {obj.qty} {obj.satuan}
+                            {" | "}
+                            {obj.tempat}
                         </p>
                     ))}
                 </CardBody>
